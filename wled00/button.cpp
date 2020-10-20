32163ba0f2275560760dbc66b10dacc0423244c9
srg74 <28492985+srg74@users.noreply.github.com>
2020-10-19 20:32:10 -0400

    Inverse for touch button

--- wled00/button.cpp
+++ wled00/button.cpp
@@ -18,7 +18,7 @@ void shortPressAction()
 bool isButtonPressed()
 {
   #ifdef BTNPIN
-    if (digitalRead(BTNPIN) == LOW) return true;
+    if (digitalRead(BTNPIN) == HIGH) return true;
   #endif
   #ifdef TOUCHPIN
     if (touchRead(TOUCHPIN) <= TOUCH_THRESHOLD) return true;
