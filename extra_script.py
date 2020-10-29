0db17e63db30733b94f03b29e4c9ded7ca4ba8f0
srg74 <serg74@hotmail.com>
2020-10-29 19:29:51 -0400

    Addded custom script

---
+++ extra_script.py
@@ -0,0 +1,25 @@
+# Import("env")
+
+# my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+# print(my_flags.get("CPPDEFINES"))
+
+# env.Replace(PROGNAME="firmware_%s" % build_tag)
+
+Import("env")
+
+version_from_buildflags = "not_found"
+my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+# some flags are just defines without value
+# some are key, value pairs
+for x in my_flags.get("CPPDEFINES"):
+    print(x)
+    if isinstance(x, list):
+        # grab as key, value
+        k, v = x
+        if k == "VERSION":
+            version_from_buildflags = v
+            build_version = "2010290"
+            # no need to iterate further
+            break
+
+env.Replace(PROGNAME="firmware_v%s_%s" % (version_from_buildflags, build_version))
