e62858a525e0878d4dca29c5a59e86d5f5814a69
srg74 <serg74@hotmail.com>
2020-10-29 20:24:03 -0400

    Added script for custom bin name

--- extra_script.py
+++ extra_script.py
@@ -12,14 +12,14 @@ my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 # some flags are just defines without value
 # some are key, value pairs
 for x in my_flags.get("CPPDEFINES"):
-    print(x)
+#   print(x)
     if isinstance(x, list):
         # grab as key, value
         k, v = x
         if k == "VERSION":
             version_from_buildflags = v
-            build_version = "2010290"
+            build_number = "2010290"
             # no need to iterate further
             break
 
-env.Replace(PROGNAME="firmware_v%s_%s" % (version_from_buildflags, build_version))
+env.Replace(PROGNAME="firmware_v%s" % (version_from_buildflags))
