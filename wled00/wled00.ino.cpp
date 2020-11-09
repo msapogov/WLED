d57ab89415de927e62a0984a1a736b3fdeee8d7e
srg74 <serg74@hotmail.com>
2020-11-09 16:10:58 -0500

    pins changed

---
+++ wled00/wled00.ino.cpp
@@ -0,0 +1,15 @@
+# 1 "/var/folders/0s/29fgdnsx1pd9p3wcjqt_3rq00000gn/T/tmpq21ri_fp"
+#include <Arduino.h>
+# 1 "/Users/sk/Documents/GitHub/WLED/wled00/wled00.ino"
+# 13 "/Users/sk/Documents/GitHub/WLED/wled00/wled00.ino"
+#include "wled.h"
+void setup();
+void loop();
+#line 15 "/Users/sk/Documents/GitHub/WLED/wled00/wled00.ino"
+void setup() {
+  WLED::instance().setup();
+}
+
+void loop() {
+  WLED::instance().loop();
+}
